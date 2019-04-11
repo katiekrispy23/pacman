4f2fc3f9d30a4d858a088a1f356464b308faa075
katiekrispy <katelynchambers@gmail.com>
2019-04-11 10:48:26 -0400

    adding pygame settings

---
+++ Functions.py
@@ -0,0 +1,6 @@
+import pygame, sys, random
+from pygame.locals import *
+from PygameSettings import *
+import math
+import time
+from random import *
--- Pacman.py
+++ Pacman.py
@@ -1,2 +1,6 @@
-import pygame
+import pygame, sys, random
+from pygame.locals import *
+from PygameSettings import *
 import math
+import time
+from random import *
