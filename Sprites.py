7362d5d4abf8ce3f56817995d13d2265f437f633
katiekrispy23 <49525289+katiekrispy23@users.noreply.github.com>
2019-04-23 19:53:20 -0400

    Delete Sprites.py

--- Sprites.py
+++
@@ -1,7 +0,0 @@
-from Classes import *
-from pygame import *
-
-blueGhost = pygame.image.load("GhostImages/BLUE_GHOST.PNG")
-redGhost = pygame.image.load("GhostImages/RED_GHOST.PNG")
-orangeGhost = pygame.image.load("GhostImages/ORANGE_GHOST.PNG")
-pinkGhost = pygame.image.load("GhostImages/PINK_GHOST.PNG")
